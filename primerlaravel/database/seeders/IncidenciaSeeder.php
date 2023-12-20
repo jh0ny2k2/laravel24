27602e63560f81e42b8e40b18821a647d8ebf371
jh0ny2k2 <jchaman2306@g.educaand.es>
2023-12-20 16:20:27 +0100

    primera subida

---
+++ primerlaravel/database/seeders/IncidenciaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Incidencia;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IncidenciaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Incidencia::factory()
+            ->count(50)
+            ->create();
+    }
+}
